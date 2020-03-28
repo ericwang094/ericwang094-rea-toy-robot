1fb271c53fae6e1e5fe519ead33eb1326c56e999
ericwang094 <eric.wang094@gmail.com>
2020-03-29 01:40:28 +1100

    remove exception

--- src/main/java/com/toy/robot/exception/ToyRobotInvalidPlaceException.java
+++
@@ -1,8 +0,0 @@
-package com.toy.robot.exception;
-
-public class ToyRobotInvalidPlaceException extends Exception {
-	public static final String INVALID_PLACE_ERR_MSG = "Ops, your robot is falling off table, please place it again";
-	public ToyRobotInvalidPlaceException(String string) {
-		super(string);
-	}
-}
