5a9286bce63e5d3598d805484093d02aa6d0cd62
ericwang094 <eric.wang094@gmail.com>
2020-03-28 22:55:14 +1100

    Input Handler with command line test

---
+++ src/main/java/com/toy/robot/inputhandler/InputHandler.java
@@ -0,0 +1,75 @@
+package com.toy.robot.inputhandler;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class InputHandler {
+	InputStream inputStream;
+
+	public InputHandler(InputStream inputStream) {
+		this.inputStream = inputStream;
+	}
+
+	/**
+	 * @return Get commands based on commandLine input
+	 */
+	public List<String> getCommands(String[] args) {
+		switch (args.length) {
+			case 0:
+				return getCommandsFromCommandLine();
+			case 1:
+				return getCommandLineFromFile(args[0]);
+			default:
+				return new ArrayList<String>();
+		}
+	}
+
+	/**
+	 * @return A list of valid commands from command line
+	 */
+	private List<String> getCommandsFromCommandLine() {
+		Scanner scanner = new Scanner(inputStream);
+		System.out.println("ToyGame started");
+		System.out.println("Please input command");
+		System.out.println("Invalid input will be ignored, CTRL + E to execute command, enjoy");
+		return filterCommandFromInput(scanner);
+	}
+
+	/**
+	 *
+	 * @param filePath This is the file path that will be used to get command
+	 * @return A list of valid command
+	 */
+	private List<String> getCommandLineFromFile(String filePath) {
+		List<String> commandList = new ArrayList<>();
+		try {
+			File file = new File(filePath);
+			Scanner scanner = new Scanner(file);
+			commandList = filterCommandFromInput(scanner);
+			return commandList;
+		} catch (IOException e) {
+			System.out.println("Cannot read content from file");
+			return commandList;
+		}
+	}
+
+	/**
+	 * @param scanner This is the scanner to get input
+	 * @return A list of valid command
+	 */
+	private List<String> filterCommandFromInput(Scanner scanner) {
+		List<String> commandList = new ArrayList<>();
+		while (scanner.hasNextLine()) {
+			String commandLine = scanner.nextLine();
+			if (!InputValidator.validateInput(commandLine)) {
+				continue;
+			}
+			commandList.add(commandLine);
+		}
+		return commandList;
+	}
+}
---
+++ src/test/java/com/toy/robot/inputhandler/InputHandlerTest.java
@@ -0,0 +1,75 @@
+package com.toy.robot.inputhandler;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class InputHandlerTest {
+	private InputHandler inputHandler;
+	private InputStream inputStream;
+
+	@BeforeEach
+	public void setup() {
+		inputHandler = new InputHandler(inputStream);
+	}
+
+	@Test
+	@DisplayName("Test get input from command line")
+	public void getCommandsTest() {
+		List<String> inputCommandList = prepareTestInputString();
+		inputHandler.inputStream = new ByteArrayInputStream(prepareTestInput(inputCommandList).getBytes());
+		List<String> commandList = inputHandler.getCommands(new String[]{});
+		List<String> benchMark = prepareTestInputString();
+		assertThat(commandList)
+				.as("Two command should be the same")
+				.isEqualTo(benchMark);
+	}
+
+	@Test
+	@DisplayName("Test get input from command line with 1 incorrect command")
+	public void getCommandsTestWithOneIncorrectCommand() {
+		List<String> inputCommandList = prepareTestInputStringWithOneInvalidCommand();
+		inputHandler.inputStream = new ByteArrayInputStream(prepareTestInput(inputCommandList).getBytes());
+		List<String> commandList = inputHandler.getCommands(new String[]{});
+		List<String> benchMark = prepareTestInputString();
+		assertThat(commandList)
+				.as("Two command should be the same")
+				.isEqualTo(benchMark);
+	}
+
+	private String prepareTestInput(List<String> testInputList) {
+		StringBuilder sb = new StringBuilder();
+
+		for (String s : testInputList) {
+			sb.append(s).append("\n");
+		}
+
+		return sb.toString();
+	}
+
+	private List<String> prepareTestInputString() {
+		List<String> list = new ArrayList<>();
+		list.add("PLACE 0,0,NORTH");
+		list.add("LEFT");
+		list.add("MOVE");
+		list.add("REPORT");
+		return list;
+	}
+
+	private List<String> prepareTestInputStringWithOneInvalidCommand() {
+		List<String> list = new ArrayList<>();
+		list.add("PLACE 0,0,NORTH");
+		list.add("LEFT");
+		list.add("asdf");
+		list.add("MOVE");
+		list.add("REPORT");
+		return list;
+	}
+}
