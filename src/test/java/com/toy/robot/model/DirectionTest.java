e55a0e29762e63e0d0f9ba5f926b384de5e3c084
ericwang094 <eric.wang094@gmail.com>
2020-03-28 14:27:28 +1100

    use AssertJ for unit test

--- src/test/java/com/toy/robot/model/DirectionTest.java
+++ src/test/java/com/toy/robot/model/DirectionTest.java
@@ -2,6 +2,7 @@ package com.toy.robot.model;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 
 public class DirectionTest {
@@ -10,17 +11,35 @@ public class DirectionTest {
 	public void testRotate() {
 		Direction direction = Direction.NORTH;
 		Direction directionEast = direction.rotateRight();
-		assertEquals("Rotate clockwise from North to East", Direction.EAST, directionEast);
+		assertThat(directionEast)
+				.as("Rotate clockwise from North to East")
+				.isEqualToComparingFieldByField(Direction.EAST);
+
 		Direction directionSouth = directionEast.rotateRight();
-		assertEquals("Rotate clockwise from East to South", Direction.SOUTH, directionSouth);
+		assertThat(directionSouth)
+				.as("Rotate clockwise from East to South")
+				.isEqualToComparingFieldByField(Direction.SOUTH);
+
 		Direction directionWest = directionSouth.rotateRight();
-		assertEquals("Rotate clockwise from South to West", Direction.WEST, directionWest);
+		assertThat(directionWest)
+				.as("Rotate clockwise from South to West")
+				.isEqualToComparingFieldByField(Direction.WEST);
+
 		Direction directionNorth = directionWest.rotateRight();
-		assertEquals("Rotate clockwise from West to North", Direction.NORTH, directionNorth);
+		assertThat(directionNorth)
+				.as("Rotate clockwise from West to North")
+				.isEqualToComparingFieldByField(Direction.NORTH);
 
 		directionWest = direction.rotateLeft();
-		assertEquals("Rotate counterclockwise from North to West", Direction.WEST, directionWest);
+		assertThat(directionWest)
+				.as("Rotate counterclockwise from North to West")
+				.isEqualToComparingFieldByField(Direction.WEST);
+
 		directionSouth = directionWest.rotateLeft();
+		assertThat(directionSouth)
+				.as("Rotate counterclockwise from West to South")
+				.isEqualToComparingFieldByField(Direction.SOUTH);
+
 		assertEquals("Rotate counterclockwise from West to South", Direction.SOUTH, directionSouth);
 		directionEast = directionSouth.rotateLeft();
 		assertEquals("Rotate counterclockwise from South to East", Direction.EAST, directionEast);
